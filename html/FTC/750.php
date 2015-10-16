0b91869fb3ae4ae4854d7d35081276eafeb1d31a
17dukelowp <17dukelowp@users.noreply.github.com>
2015-10-15 19:22:39 -0700

    Added team roster
    Also fixed title and began building page content.

--- html/FTC/750.php
+++ html/FTC/750.php
@@ -4,13 +4,32 @@
 <head>
 <link rel="icon" href="your_icon_goes_here.ico" type="image/x-icon">
 <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-<title>750</title>
+<title>FTC Team 750</title>
 <meta http-equiv="Content-Language" content="en-us" />
-<meta name="author" content="Your Name Goes Here" />
+<meta name="author" content="Dukelow" />
 <style type="text/css" media="all">
 </style>
 </head>
 <body>
 <?php include('menu.php'); ?>
+		<div id="content">
+			<h2>Team 750</h2>
+			<ul id="team">
+				<li>Darby Cox</li>
+				<li>Cameron Doughty</li>
+				<li>Ethan Gazin</li>
+				<li>Ryan Hardinge</li>
+				<li>Julian Lethbridge</li>
+				<li>McGuire Meek</li>
+				<li>Jesus Mendoza</li>
+				<li>Cao-duy Nguyen</li>
+				<li>Hoang Nguyen</li>
+				<li>Daniel Smotkin</li>
+				<li>Tyler Walker</li>
+				<li id="coach">Coaches: Mr. Doberenz and Mr. Cox</li>
+			</ul>
+			<br />
+		</div>
+</html>
 </body>
 </html>
