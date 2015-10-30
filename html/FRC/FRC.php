c907308d28258f532298dcd4e81bf351ef0b13d7
fluteguy400 <fluteguy400@gmail.com>
2015-10-29 18:17:32 -0700

    Added roster

--- html/FRC/FRC.php
+++ html/FRC/FRC.php
@@ -30,7 +30,7 @@ blockquote {float:right;}
 
 <?php include('menu.php'); ?>
 <div class="maint">
-<h2>Team 3192 - TigerBytes</h2>
+<h2 style="height:50px;">Team 3192 - TigerBytes</h2>
 
 <blockquote><i>"The varsity Sport for the Mind," FRC combines the excitement of sport with the rigors of science and technology. </br>
 Under strict rules, limited resources, and time limits, teams of 25 students or more are challenged to raise funds, design </br>
@@ -38,12 +38,50 @@ a team "brand," hone teamwork skills, and build and program robots to perform pr
 competitors. It's as close to "real-world engineering" as a student can get. Volunteer professional mentors lend their </br>
 time and talents to guide each team.</i></blockquote>
 
-<p>Except for when they had split into two teams for BunnyBots, this is the only team representing Tigard High School in the FRC league.</p>
 
 </hr>
-<h3><i>This year's game</i></h3>
+<!--<h3><i>This year's game</i></h3>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/hTyCIYZQ_1s" frameborder="0" allowfullscreen></iframe>
-<p>On Saturday after the game release, we met in TNET, downloaded the Game Manual, made copies for each of the specialty teams and began to read. By noon when the rest of the FRC Team gathered for their reveal, we had inventoried the KoP, laid it out for inspection by the team, read the Game Manual and developed a brief demonstration to further explain the FIRST video. With the majority of the team on hand, we watched the video once more, demonstrated the 2015 field elements and passed out the Game Manual copies. We then spent an hour reading the entire Game Manual in pairs, marking the copies with questions for further discussion. When everyone had a firm grasp of the game, we divided into three teams. Each team was tasked with developing a basic robot design based on their proposed game strategy. After thirty minutes of brainstorming, the three teams came back together and presented their ideas to the rest of the group. The teams all came to the same general idea- a maneuverable chassis with a fork-lift like devise on the front end to move and stack the boxes. At the completion of the presentations, each team leader was asked to "formalize" their ideas for presentation that Tuesday night to the assembled coaches and teammates, concentrating specifically on design capabilities and limitations.</p>
+<p>On Saturday after the game release, we met in TNET, downloaded the Game Manual, made copies for each of the specialty 
+teams and began to read. By noon when the rest of the FRC Team gathered for their reveal, we had inventoried the KoP,
+laid it out for inspection by the team, read the Game Manual and developed a brief demonstration to further explain the 
+FIRST video. With the majority of the team on hand, we watched the video once more, demonstrated the 2015 field elements 
+and passed out the Game Manual copies. We then spent an hour reading the entire Game Manual in pairs, marking the copies
+with questions for further discussion. When everyone had a firm grasp of the game, we divided into three teams. Each team 
+was tasked with developing a basic robot design based on their proposed game strategy. After thirty minutes of brainstorming
+, the three teams came back together and presented their ideas to the rest of the group. The teams all came to the same 
+general idea- a maneuverable chassis with a fork-lift like devise on the front end to move and stack the boxes. At the
+completion of the presentations, each team leader was asked to "formalize" their ideas for presentation that Tuesday night
+to the assembled coaches and teammates, concentrating specifically on design capabilities and limitations.</p>-->
+<h1>The Team</h1>
+<ul>
+<li>Odalis Angeles</li>
+<li>Johannes Bromander</li>
+<li>Stacy Torres</li>
+<li>Jeffrey Chapman</li>
+<li>Tiffany Chu</li>
+<li>Trinity Deic</li>
+<li>Patrick Dukelow</li>
+<li>Dan Field</li>
+<li>Jonathan Gillespie</li>
+<li>Luong Hoang</li>
+<li>Jordan Holland</li>
+<li>Jason Holm</li>
+<li>Jacob Jones</li>
+<li>Samantha Perona</li>
+<li>Sameer Raviprakash</li>
+<li>Ian Roberts</li>
+<li>Peter Strobel</li>
+<li>Cole Swanson</li>
+<li>Jonathan Le</li>
+<li>Abbey Lewis</li>
+<li>Austin Liu</li>
+<li>Kenny Luong</li>
+<li>Austin Mckee</li>
+<li>Xavier Mendez</li>
+<li>Drew Ortega</li>
+</ul>
+
 </div>
 </body>
 
