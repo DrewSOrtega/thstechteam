2226a29e10b944d7111b7b407ea32c32c68d7668
Drew S. Ortega <Drewsortega@gmail.com>
2015-12-11 07:59:08 -0800

    Update Charlie.php

--- html/LoL/Charlie.php
+++ html/LoL/Charlie.php
@@ -2,7 +2,7 @@
 <html>
 <head><meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
 
-	<title>Alpha</title>
+	<title>Charlie</title>
 
 	<meta http-equiv="Content-Language" content="en-us" />
 	<meta name="author" content="Your Name Goes Here" />
