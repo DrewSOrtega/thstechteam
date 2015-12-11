fe47d84f41823c0b4f443bbdcab0b5ddb308c386
Drew S. Ortega <Drewsortega@gmail.com>
2015-12-11 08:06:24 -0800

    Update courses.php

--- html/Programming/courses.php
+++ html/Programming/courses.php
@@ -1,18 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<link rel="icon" href="your_icon_goes_here.ico" type="image/x-icon">
-<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-<title>Courses</title>
-<meta http-equiv="Content-Language" content="en-us" />
-<meta name="author" content="Your Name Goes Here" />
-<link rel="stylesheet" type="text/css" href="../../css/css.css">
-<style type="text/css" media="all">
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+
+	<title>Charlie</title>
+
+	<meta http-equiv="Content-Language" content="en-us" />
+	<meta name="author" content="Your Name Goes Here" />
+	<link rel="shortcut icon" href="../../image/powert.png">
+	<link rel="stylesheet" type="text/css" href="../../css/css.css">
+	<link rel="stylesheet" type="text/css" href="../../css/navbar.css">
+	<style type="text/css" media="all">
+h1{ text-align:center;
+	color:black;
+	font-weight:bold;
+	font-size: 50px;
+}
+img{
+	margin:auto;
+	float:left;
+}
 </style>
 </head>
 <body>
-<?php include('menu.php'); ?>
+<?php include('../menu.php'); ?>
 <div class="maint">
 <ul>
 	<li>Programming Courses</li>
