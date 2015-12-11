0e800710bcf9da289fe9b5e7d12211c85283ac2f
fluteguy400 <fluteguy400@gmail.com>
2015-12-11 07:56:12 -0800

    fixed menu.php

--- html/Home/calendar.php
+++ html/Home/calendar.php
@@ -9,7 +9,7 @@
 </style>
 </head>
 <body>
-<?php include('menu.php'); ?>
+<?php include('../menu.php'); ?>
 <div style="margin:auto; width:90%; height:700px;">
 <iframe src="https://www.google.com/calendar/embed?src=qip9g4lbpp59sdoa3l5f2o1qi4%40group.calendar.google.com&ctz=America/Los_Angeles" width="100%" height="700px"></iframe>
 </div>
