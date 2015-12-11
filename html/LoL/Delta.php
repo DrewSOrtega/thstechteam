d7915c9b17f365c501ea9a45d1c9c4d59befdd5e
Drew S. Ortega <Drewsortega@gmail.com>
2015-12-11 07:59:18 -0800

    Update Delta.php

--- html/LoL/Delta.php
+++ html/LoL/Delta.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
- 
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title>Page Title Goes Here</title>
-	</head>
- 
-	<body>
-		<!-- Page content goes here -->
-	</body>
- 
+	<title>Alpha</title>
+
+	<meta http-equiv="Content-Language" content="en-us" />
+	<meta name="author" content="Your Name Goes Here" />
+	<link rel="shortcut icon" href="../../image/powert.png">
+	<link rel="stylesheet" type="text/css" href="../../css/css.css">
+	<link rel="stylesheet" type="text/css" href="../../css/navbar.css">
+	<style type="text/css" media="all">
+h1{ text-align:center;
+	color:black;
+	font-weight:bold;
+	font-size: 50px;
+}
+img{
+	margin:auto;
+	float:left;
+}
+</style>
+</head>
+<body>
+<?php include('../menu.php'); ?>
+</div>
+</div>
+</body>
 </html>
