42ac863bf59a083478d634a271acf2e252eb6539
fluteguy400 <fluteguy400@gmail.com>
2015-12-10 18:28:35 -0800

    Update home.php

--- html/Home/home.php
+++ html/Home/home.php
@@ -1,1 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
 
+	<title>Home</title>
+
+	<meta http-equiv="Content-Language" content="en-us" />
+	<meta name="author" content="Your Name Goes Here" />
+	<link rel="shortcut icon" href="../../image/powert.png">
+	<link rel="stylesheet" type="text/css" href="../../css/css.css">
+	<style type="text/css" media="all">
+h1{ text-align:center;
+	color:black;
+	font-weight:bold;
+	font-size: 50px;
+}
+img{
+	margin:auto;
+	float:left;
+}
+</style>
+</head>
+<body>
+<?php include('../../menu.php'); ?>
+<br/><br/>
+<h1>Tigard High School Technology Team</h1>
+<div class="maint">
+Tigard high has one of the largest and most diverse technology teams in the state. The students compete in many events throughout the year.
+We compete in programming and robotics, with our team consisting of over fifty members. The Robotics portion of our team includes four FTC (4097, 3965, 3058 and 750) and two FRC teams. We recently formed a SeaPerch team and will be competing this year. We also introduced a League of Legends program. So far we have five teams competing online on the North American server, high school division. They are competing for prize money.
+Our FRC team has split into two smaller teams in order to tackle our next big event, BunnyBots. The division of labor will give us a great head start to this years challenge.
+<br/><br/>
+
+<br/><br/>
+Tigard also has a very expansive Information Technology course selection. With over six Microsoft certifications offered, 
+the students leave with plenty of training. We arleady earned over 60 certifications this fall, and Mentor Graphics helping to pay for this years certifications, we are looking forward the upcoming season.
+<br/></br>
+<img src="../../image/Lam_logo.png" width="33%"/>
+<img src="../../image/mentor_logo.gif" width="33%"/>
+<br><br><br><br><br>
+<h1>News:</h1><br>
+<div style="height:300px;">
+<!--Add all news in this div-->	
+</div>
+</div>
+</body>
+</html>
