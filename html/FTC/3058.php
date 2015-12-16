2a92487521df7fe52d8340152c06351d1d8cb820
fluteguy400 <fluteguy400@gmail.com>
2015-12-15 18:16:36 -0800

    added photo

--- html/FTC/3058.php
+++ html/FTC/3058.php
@@ -17,7 +17,7 @@
 			<h1>FTC Team 3058</h1>
 			<ul>
 			<h2>Roster</h2>
-			<img src="../../image/1213robotphoto.jpg" width=50% height=auto style="float: right;"/> 
+			<img src="../../image/ftcpics/3058.jpg" width=50% height=auto style="float: right;"/> 
 				<li>Jaun Andreos</li>
 				<li>Emma Barbee</li>
 				<li>Jan Carlson</li>
