b7106042a2fd6c3051158d23d62870b268481242
fluteguy400 <fluteguy400@gmail.com>
2016-01-07 18:19:12 -0800

    Update menu.php

--- html/menu.php
+++ html/menu.php
@@ -15,56 +15,56 @@
 		<div class="background">
 			<div class="navbar">
 					<div class="navbuttHome">
-						<a href="../Home/home.php" class="linkage" target="bucky">Home</a>
+						<a href="../Home/home.php" class="linkage" target="_self">Home</a>
 						</br></br>
-						<a href="../Home/history.php" class="linkage" target="bucky">History</a>
+						<a href="../Home/history.php" class="linkage" target="_self">History</a>
 						</br></br>
-						<a href="../Home/calendar.php" class="linkage" target="bucky">Calendar</a>
+						<a href="../Home/calendar.php" class="linkage" target="_self">Calendar</a>
 					</div>
 					<div class="navbuttFRC">
-						<a href="../FRC/FRC.php" class="linkage" target="bucky">FRC</a>
+						<a href="../FRC/FRC.php" class="linkage" target="_self">FRC</a>
 						</br></br>
-						<a href="../FRC/3192A.php" class="linkage" target="bucky">3192A</a>
+						<a href="../FRC/3192A.php" class="linkage" target="_self">3192A</a>
 						</br></br>
-						<a href="../FRC/3192B.php" class="linkage" target="bucky">3192B</a>	
+						<a href="../FRC/3192B.php" class="linkage" target="_self">3192B</a>	
 					</div>
 						<div class="navbuttFTC">
-						<a href="../FTC/FTCindex.php" class="linkage" target="bucky">FTC</a>
+						<a href="../FTC/FTCindex.php" class="linkage" target="_self">FTC</a>
 						</br></br>
-						<a href="../FTC/750.php" class="linkage" target="bucky">750</a>
+						<a href="../FTC/750.php" class="linkage" target="_self">750</a>
 						</br></br>
-						<a href="../FTC/3058.php" class="linkage" target="bucky">3058</a>
+						<a href="../FTC/3058.php" class="linkage" target="_self">3058</a>
 						</br></br>
-						<a href="../FTC/3965.php" class="linkage" target="bucky">3965</a>
+						<a href="../FTC/3965.php" class="linkage" target="_self">3965</a>
 						</br></br>
-						<a href="../FTC/4097.php" class="linkage" target="bucky">4097</a>						
+						<a href="../FTC/4097.php" class="linkage" target="_self">4097</a>						
 					</div>
 					<div class="navbuttProgramming">
-						<a href="../Programming/Progindex.php" class="linkage" target="bucky">Programming</a>
+						<a href="../Programming/Progindex.php" class="linkage" target="_self">Programming</a>
 						</br></br>
-						<a href="../Programming/OGPC.php" class="linkage" target="bucky">OGPC</a>
+						<a href="../Programming/OGPC.php" class="linkage" target="_self">OGPC</a>
 						</br></br>
-						<a href="../Programming/willamette.php" class="linkage" target="bucky">Willamette</a>
+						<a href="../Programming/willamette.php" class="linkage" target="_self">Willamette</a>
 						</br></br>
-						<a href="../Programming/courses.php" class="linkage" target="bucky">Courses</a>
+						<a href="../Programming/courses.php" class="linkage" target="_self">Courses</a>
 					</div>
 					<div class="navbuttLoL">
-						<a href="../LoL/lolindex.php" class="linkage" target="bucky">League of Legends</a>
+						<a href="../LoL/lolindex.php" class="linkage" target="_self">League of Legends</a>
 						</br></br>
-						<a href="../LoL/Alpha.php" class="linkage" target="bucky">Alpha</a>
+						<a href="../LoL/Alpha.php" class="linkage" target="_self">Alpha</a>
 						</br></br>
-						<a href="../LoL/Bravo.php" class="linkage" target="bucky">Bravo</a>
+						<a href="../LoL/Bravo.php" class="linkage" target="_self">Bravo</a>
 						</br></br>
-						<a href="../LoL/Charlie.php" class="linkage" target="bucky">Charlie</a>
+						<a href="../LoL/Charlie.php" class="linkage" target="_self">Charlie</a>
 						</br></br>
-						<a href="../LoL/Delta.php" class="linkage" target="bucky">Delta</a>
+						<a href="../LoL/Delta.php" class="linkage" target="_self">Delta</a>
 						</br></br>
-						<a href="../LoL/Echo.php" class="linkage" target="bucky">Echo</a>
+						<a href="../LoL/Echo.php" class="linkage" target="_self">Echo</a>
 					</div>
 				</div>
 			</div>
 		</div>
-<!--<iframe scrolling="yes" name="bucky"></iframe>-->
+<!--<iframe scrolling="yes" name="_self"></iframe>-->
 <marquee direction="left" scrollamount="4px">
 <font size="+2" color="gold">Lam Research - $12000&nbsp;&nbsp;</font>
 <font size="+2" color="gold">Oregon Department of Education - $7000&nbsp;&nbsp</font>
