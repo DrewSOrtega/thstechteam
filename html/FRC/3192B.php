30070bd7e00fcc05386b7849ebfc52eb84f6a784
Drew S. Ortega <Drewsortega@gmail.com>
2016-01-07 17:46:37 -0800

    actually fixed alswel

--- html/FRC/3192B.php
+++ html/FRC/3192B.php
@@ -1,22 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
-<head>
-
-	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
 
 	<title>3192B</title>
 
 	<meta http-equiv="Content-Language" content="en-us" />
 	<meta name="author" content="Your Name Goes Here" />
+	<link rel="shortcut icon" href="../../image/powert.png">
+	<link rel="stylesheet" type="text/css" href="../../css/css.css">
+	<link rel="stylesheet" type="text/css" href="../../css/navbar.css">
 	<style type="text/css" media="all">
-
+h1{ text-align:center;
+	color:black;
+	font-weight:bold;
+	font-size: 50px;
+}
+img{
+	margin:auto;
+	float:left;
+}
 </style>
-
 </head>
-
 <body>
 <?php include('../menu.php'); ?>
 
