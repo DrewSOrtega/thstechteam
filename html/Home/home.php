85f32a1fecce86ab63a31ca33f7be211cf932fd2
Reza Fouladian <scgaegavga@gmail.com>
2016-01-08 01:49:48 -0800

    Added background to text on home page for easier reading

--- html/Home/home.php
+++ html/Home/home.php
@@ -26,6 +26,7 @@ img{
 <br/><br/>
 <h1>Tigard High School Technology Team</h1>
 <div class="maint">
+<div class="bodytext">
 Tigard high has one of the largest and most diverse technology teams in the state. The students compete in many events throughout the year.
 We compete in programming and robotics, with our team consisting of over fifty members. The Robotics portion of our team includes four FTC (4097, 3965, 3058 and 750) and two FRC teams. We recently formed a SeaPerch team and will be competing this year. We also introduced a League of Legends program. So far we have five teams competing online on the North American server, high school division. They are competing for prize money.
 Our FRC team has split into two smaller teams in order to tackle our next big event, BunnyBots. The division of labor will give us a great head start to this years challenge.
@@ -34,6 +35,7 @@ Our FRC team has split into two smaller teams in order to tackle our next big ev
 <br/><br/>
 Tigard also has a very expansive Information Technology course selection. With over six Microsoft certifications offered, 
 the students leave with plenty of training. We arleady earned over 60 certifications this fall, and Mentor Graphics helping to pay for this years certifications, we are looking forward the upcoming season.
+</div>
 <br/></br>
 <img src="../../image/Lam_logo.png" width="33%"/>
 <img src="../../image/mentor_logo.gif" width="33%"/>
