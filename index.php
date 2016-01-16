6529c8b8f5d9568a7546b8a88b4373a34ab5d592
Reza Fouladian <scgaegavga@gmail.com>
2016-01-16 12:30:18 -0800

    Changed html redirect to a PHP 302 redirect

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
-<head>
-
-	<!--<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />-->
-        <meta http-equiv="refresh" content="0; url=http://thstechteam.org/html/Home/home.php" />
-	<title>Redirecting...</title>
-
-	<meta http-equiv="Content-Language" content="en-us" />
-	<meta name="author" content="Your Name Goes Here" />
-	<style type="text/css" media="all">
-
-</style>
-
-</head>
-
-<body>
-
-</body>
-
-</html>
+<?php 
+header("HTTP/1.1 302 Moved Temporarily"); 
+header("Location: http://thstechteam.org/html/Home/home.php"); 
+?>
