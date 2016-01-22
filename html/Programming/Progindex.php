7e04a53fda5aa2902a79eb92f89cc82f0e5ea4e0
Reza Fouladian <reza.fouladian@pcc.edu>
2016-01-22 11:32:22 -0800

    Redirect progindex to courses. Fixes #5
    Redirecting until content is added

--- html/Programming/Progindex.php
+++ html/Programming/Progindex.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head><meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-
-	<title>Programming</title>
-
-	<meta http-equiv="Content-Language" content="en-us" />
-	<meta name="author" content="Your Name Goes Here" />
-	<link rel="shortcut icon" href="../../image/powert.png">
-	<link rel="stylesheet" type="text/css" href="../../css/css.css">
-	<link rel="stylesheet" type="text/css" href="../../css/navbar.css">
-	<style type="text/css" media="all">
-h1{ text-align:center;
-	color:black;
-	font-weight:bold;
-	font-size: 50px;
-}
-img{
-	margin:auto;
-	float:left;
-}
-</style>
-</head>
-<body>
-<?php include('../menu.php'); ?>
-<div class="bodytext"></div>
-</body>
-</html>
+<?php 
+header("HTTP/1.1 302 Moved Temporarily"); 
+header("Location: http://thstechteam.org/html/Programming/courses.php"); 
+?>
