97c27db6a0d30d97ca0d875174c20649dae340dd
Drew S. Ortega <Drewsortega@gmail.com>
2016-02-02 18:39:40 -0800

    Update Alpha.php

--- html/LoL/Alpha.php
+++ html/LoL/Alpha.php
@@ -23,5 +23,74 @@ img{
 </head>
 <body>
 <?php include('../menu.php'); ?>
+<div class="bodytext maint">
+<h2>Group G Competition Results</h2>
+<table border="1">
+	<tr>
+		<th>Week</th>
+		<th>Win/Loss</th>
+		<th>Challenger</th>
+	</tr>
+	<tr>
+		<td>Week 1</td>
+		<td>Win</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 2</td>
+		<td>Win</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 3</td>
+		<td>Loss</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 4</td>
+		<td>Win</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 5</td>
+		<td>Win</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 6</td>
+		<td>Loss</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 7</td>
+		<td>Win</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 8</td>
+		<td>Loss</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Week 9</td>
+		<td>Win</td>
+		<td>Concord High</td>
+	</tr>
+	<tr>
+		<td>Place</td>
+		<td colspan="2">8th/88</td>
+	</tr>
+</table>
+<h2>Roster</h2>
+<ul>
+  <li>Austin Liu (Team Captain)</li>
+  <li>Peter Kwak</li>
+  <li>Evan Consani</li>
+  <li>Kevin Olson</li>
+  <li>Justin Tran</li>
+  <li>Blake Le</li>
+  <li>Alex Foroughi</li>
+</ul>
+</div>
 </body>
 </html>
