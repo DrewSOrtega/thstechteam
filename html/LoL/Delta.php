0803fc8da5439c3abc8f83152fc95501fb24e487
Drew S. Ortega <Drewsortega@gmail.com>
2016-02-02 18:48:01 -0800

    Update Delta.php

--- html/LoL/Delta.php
+++ html/LoL/Delta.php
@@ -23,5 +23,74 @@ img{
 </head>
 <body>
 <?php include('../menu.php'); ?>
+<div class="bodytext maint">
+<h2>Group H Competition Results</h2>
+<table border="1">
+	<tr>
+		<th>Week</th>
+		<th>Win/Loss</th>
+		<th>Challenger</th>
+	</tr>
+	<tr>
+		<td>Week 1</td>
+		<td>Loss</td>
+		<td>Fossil Ridge High</td>
+	</tr>
+	<tr>
+		<td>Week 2</td>
+		<td>Loss</td>
+		<td>Independence High</td>
+	</tr>
+	<tr>
+		<td>Week 3</td>
+		<td>Loss</td>
+		<td>Charter Oak HS</td>
+	</tr>
+	<tr>
+		<td>Week 4</td>
+		<td>Loss</td>
+		<td>Hilmar High</td>
+	</tr>
+	<tr>
+		<td>Week 5</td>
+		<td>Loss</td>
+		<td>Central City Value High</td>
+	</tr>
+	<tr>
+		<td>Week 6</td>
+		<td>Loss</td>
+		<td>High Tech High</td>
+	</tr>
+	<tr>
+		<td>Week 7</td>
+		<td>Loss</td>
+		<td>Brighton High B</td>
+	</tr>
+	<tr>
+		<td>Week 8</td>
+		<td>Loss</td>
+		<td>Bellevue International</td>
+	</tr>
+	<tr>
+		<td>Week 9</td>
+		<td>Loss</td>
+		<td>East High</td>
+	</tr>
+	<tr>
+		<td>Place</td>
+		<td colspan="2">76th/90</td>
+	</tr>
+</table>
+<h2>Roster</h2>
+<ul>
+  <li>Ian Lewis (Team Captain)</li>
+  <li>Hunter Dietz</li>
+  <li>Alex Huynh</li>
+  <li>Elias Erickson</li>
+  <li>Austin Dufort</li>
+  <li>Lawson Dietz</li>
+  <li>Marco Predovic</li>
+</ul>
+</div>
 </body>
 </html>
