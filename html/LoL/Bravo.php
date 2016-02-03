70e2724b2ba6ca1640ba1c6d4c223af9feebcdde
Drew S. Ortega <Drewsortega@gmail.com>
2016-02-02 18:37:49 -0800

    Update Bravo.php

--- html/LoL/Bravo.php
+++ html/LoL/Bravo.php
@@ -23,5 +23,74 @@ img{
 </head>
 <body>
 <?php include('../menu.php'); ?>
+<div class="bodytext">
+<h2>Group F Competition Results</h2>
+<table border="1">
+	<tr>
+		<th>Week</th>
+		<th>Win/Loss</th>
+		<th>Challenger</th>
+	</tr>
+	<tr>
+		<td>Week 1</td>
+		<td>Loss</td>
+		<td>Olympus High</td>
+	</tr>
+	<tr>
+		<td>Week 2</td>
+		<td>Loss</td>
+		<td>Buckeye Union High A</td>
+	</tr>
+	<tr>
+		<td>Week 3</td>
+		<td>Win</td>
+		<td>Brighton High B</td>
+	</tr>
+	<tr>
+		<td>Week 4</td>
+		<td>Loss</td>
+		<td>Bishop Kelly High</td>
+	</tr>
+	<tr>
+		<td>Week 5</td>
+		<td>Win</td>
+		<td>Independent High C</td>
+	</tr>
+	<tr>
+		<td>Week 6</td>
+		<td>Loss</td>
+		<td>Lee Williams High</td>
+	</tr>
+	<tr>
+		<td>Week 7</td>
+		<td>Loss</td>
+		<td>Hamilton High C</td>
+	</tr>
+	<tr>
+		<td>Week 8</td>
+		<td>Win</td>
+		<td>International Polytechnic High A</td>
+	</tr>
+	<tr>
+		<td>Week 9</td>
+		<td>Win</td>
+		<td>University High B</td>
+	</tr>
+	<tr>
+		<td>Place</td>
+		<td colspan="2">36th/90</td>
+	</tr>
+</table>
+<h2>Roster</h2>
+<ul>
+  <li>Kenny Luong (Team Captain)</li>
+  <li>Jason Holm</li>
+  <li>Jordan Holland</li>
+  <li>Elsa Schuman-Ianier</li>
+  <li>Isaiah Spahan</li>
+  <li>Isaiah Hollingsworth</li>
+  <li>Byran Smith</li>
+</ul>
+</div>
 </body>
 </html>
