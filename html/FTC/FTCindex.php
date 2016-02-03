4bc1fa81f9f202d91c750c639f28449b201fa166
Drew S. Ortega <Drewsortega@gmail.com>
2016-02-02 17:57:03 -0800

    added a link to the news

--- html/FTC/FTCindex.php
+++ html/FTC/FTCindex.php
@@ -27,13 +27,13 @@ img{
 <h1>FTC</h1>
 <div class="maint">
 	<div class="bodytext">
-FTC is designed for those who want to compete head to head, using a sports model. Teams of up to ten 
+	<a href="http://thstechteam.org/html/Home/home.php">Click here to read about FTC's recent achievents.</a>
+<p>FTC is designed for those who want to compete head to head, using a sports model. Teams of up to ten 
 students are responsible for designing, building, and programming their robots to compete in an alliance
  format against other teams. The robot kit is reusable from year-to-year and is programmed using a variety 
  of languages. Teams, including coaches, mentors and volunteers, are required to develop strategy and build 
  robots based on sound engineering principles. Awards are given for the competition as for well as for 
- community outreach, design, and other real-world accomplishments.
- <p>
+ community outreach, design, and other real-world accomplishments.</p>
  <div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
 <iframe src="http://www.youtube.com/embed/aDJMn4-2Fp4?rel=0" allowfullscreen frameborder="0" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
 </div>
