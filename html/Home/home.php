362fd368eabb758e6d28185b671e4a4daafcfb06
Drew S. Ortega <Drewsortega@gmail.com>
2016-02-04 17:44:36 -0800

    Update home.php

--- html/Home/home.php
+++ html/Home/home.php
@@ -25,7 +25,52 @@ img{
 <?php include('../menu.php'); ?>
 <br/><br/>
 <h1>Tigard High School Technology Team</h1>
-<div class="maint">
+<div class="maint"><div class="bodytext">
+	<h1>News:</h1>
+	<br>
+	<br>
+	<p>Over the weekend, our four FTC teams competed against 20 other teams from the Portland and Beaverton Leagues
+ 	to see who would advance to the next round of competition, the Oregon Super-Qualifier Tournaments
+ 	13-14FEB enroute to the Oregon State Championships 27-28FEB.  Representing Tigard High School were:
+ 	Team 750: Gears in Motion, Team 3058: The Jury Riggers, Team 3965: Twisted Metal, Team 4097: The Rookies</p>
+ 
+	<p>Our teams placed very well, with our highest being 750 finishing 4th overall.  3965 held on to their 7th place ranking. 
+	3058 placed 9th overall.  4097 placed in 19th.</p>
+
+	<p>Based on there fourth place seeding, 750 became fourth Alliance Team Captain, choosing 3965 as 
+	their first alliance partner and another high school team as their third and final partner.
+	Team 3058 was chosen as the third alliance partner for the second Alliance team. 
+	Team 4097 was not chosen and therefore was eliminated from tournament play.</p>
+
+	<p>Alliance One vs Alliance Four started off as expected with one defeating four in the best two of three format.
+	In the second round, the team of 750/3965 pulled the upset, knocking off the first seed.  In the final and deciding
+	round, 750/3965 appeared to defeat the first alliance again.  After a significant delay while the referees measured
+	and argued and counted, the final score found 750/3965 on the wrong end of the thinnest of margins, 247-242.   750and
+	3965 were eliminated from the tournament.Meanwhile, Alliance Two with 3058 took on Alliance Three.  The alliance team
+	captain elected to sit 3058 in the first of three matches and found themselves thrashed by the third seed.
+	The team captain then elected to have his partners take the field without him and in consecutive matches,
+	A2 defeated A3 and 3058 and their partners advanced to the finals versus Alliance One.
+	Again, the A2 Captain took the field, again sitting 3058.  Again they were defeated by the other alliance.
+	The A2 Captain knew he was the weak link and played the last two matches with 3058 and a team from Wilson
+	High School.  They easily won the second match but in a close, and somewhat controversial third
+	and deciding match, A2 and 3058 defeated A1 for the League Championship Tournament title.   
+	</p>
+
+	<p>
+	At the end of the day, the teams settled in for the Award Ceremony.  While some teams advance based on their
+	field play, others advance due to their body of work based on the robot, the team's success, the design, the
+	software programming, community outreach and the team presentation.  The first award handed out was the Connect
+	Award which recognizes a team for the best use of sensors and programming to defeat the problem.  3965 was one
+	of three nominees and brought home the trophy.  The next award was the Motivate Award which recognizes a team
+	for the advancement of STEM and FIRST Robotics in their school and community.  3058 was one of three nominees
+	and brought home the trophy.  We did not win any of the other category awards but we did collect one more trophy-
+	3058 picked up another for their title win. In addition to the team awards, four of our teammates competed for the
+	Dean's List Award for individual achievement.  This year's nominees included Ethan Gazin, Emma Barbee,
+	Josh Hancock, and Grace Harestad.  All four had extensive written endorsements to support their nominations
+	and during the morning judging, met with a board of three judges for interviews.  At the end of the day, all
+	four Tigard nominees were selected to advance to the State Championship level 27-28FEB.  </p>
+</div>
+<br>
 <div class="bodytext">
 Tigard high has one of the largest and most diverse technology teams in the state. The students compete in many events throughout the year.
 We compete in programming and robotics, with our team consisting of over fifty members. The Robotics portion of our team includes four FTC (4097, 3965, 3058 and 750) and two FRC teams. We recently formed a SeaPerch team and will be competing this year. We also introduced a League of Legends program. So far we have five teams competing online on the North American server, high school division. They are competing for prize money.
@@ -40,53 +85,9 @@ the students leave with plenty of training. We already earned over 60 certificat
 <img src="../../image/mentor_logo.gif" width="33%"/>
 <img src="../../image/odot_logo.jpg" width="33%"/>
 <br><br><br><br><br>
-<h1>News:</h1><br>
 
-<div class="bodytext">
-<p>Over the weekend, our four FTC teams competed against 20 other teams from the Portland and Beaverton Leagues
- to see who would advance to the next round of competition, the Oregon Super-Qualifier Tournaments
- 13-14FEB enroute to the Oregon State Championships 27-28FEB.  Representing Tigard High School were:
- Team 750: Gears in Motion, Team 3058: The Jury Riggers, Team 3965: Twisted Metal, Team 4097: The Rookies</p>
- 
-<p>Our teams placed very well, with our highest being 750 finishing 4th overall.  3965 held on to their 7th place ranking. 
-3058 placed 9th overall.  4097 placed in 19th.</p>
 
-<p>Based on there fourth place seeding, 750 became fourth Alliance Team Captain, choosing 3965 as 
-their first alliance partner and another high school team as their third and final partner.
-Team 3058 was chosen as the third alliance partner for the second Alliance team. 
-Team 4097 was not chosen and therefore was eliminated from tournament play.</p>
 
-<p>Alliance One vs Alliance Four started off as expected with one defeating four in the best two of three format.
-In the second round, the team of 750/3965 pulled the upset, knocking off the first seed.  In the final and deciding
-round, 750/3965 appeared to defeat the first alliance again.  After a significant delay while the referees measured
-and argued and counted, the final score found 750/3965 on the wrong end of the thinnest of margins, 247-242.   750and
-3965 were eliminated from the tournament.Meanwhile, Alliance Two with 3058 took on Alliance Three.  The alliance team
-captain elected to sit 3058 in the first of three matches and found themselves thrashed by the third seed.
-The team captain then elected to have his partners take the field without him and in consecutive matches,
-A2 defeated A3 and 3058 and their partners advanced to the finals versus Alliance One.
-Again, the A2 Captain took the field, again sitting 3058.  Again they were defeated by the other alliance.
-The A2 Captain knew he was the weak link and played the last two matches with 3058 and a team from Wilson
-High School.  They easily won the second match but in a close, and somewhat controversial third
-and deciding match, A2 and 3058 defeated A1 for the League Championship Tournament title.   
-</p>
-
-<p>
-At the end of the day, the teams settled in for the Award Ceremony.  While some teams advance based on their
-field play, others advance due to their body of work based on the robot, the team's success, the design, the
-software programming, community outreach and the team presentation.  The first award handed out was the Connect
-Award which recognizes a team for the best use of sensors and programming to defeat the problem.  3965 was one
-of three nominees and brought home the trophy.  The next award was the Motivate Award which recognizes a team
-for the advancement of STEM and FIRST Robotics in their school and community.  3058 was one of three nominees
-and brought home the trophy.  We did not win any of the other category awards but we did collect one more trophy-
-3058 picked up another for their title win. In addition to the team awards, four of our teammates competed for the
-Dean's List Award for individual achievement.  This year's nominees included Ethan Gazin, Emma Barbee,
-Josh Hancock, and Grace Harestad.  All four had extensive written endorsements to support their nominations
-and during the morning judging, met with a board of three judges for interviews.  At the end of the day, all
-four Tigard nominees were selected to advance to the State Championship level 27-28FEB.  </p>
-</div>
-<div style="height:300px;">
-<!--Add all news in this div-->	
-</div>
 </div>
 </body>
 </html>
