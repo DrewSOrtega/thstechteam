de3e3007af030d38c33237c918ca8b8c6c2ef077
fluteguy400 <fluteguy400@gmail.com>
2016-02-18 18:22:00 -0800

    Updated news

--- html/Home/home.php
+++ html/Home/home.php
@@ -28,52 +28,87 @@ img{
 <div class="maint"><div class="bodytext">
 	<div class="maint-left">
 		<h2 align="left">News:</h2>
-		<p>Over the weekend, our four FTC teams competed against 20 other teams from the Portland and Beaverton Leagues
- 		to see who would advance to the next round of competition, the Oregon Super-Qualifier Tournaments
- 		13-14FEB enroute to the Oregon State Championships 27-28FEB.  Representing Tigard High School were:
- 		Team 750: Gears in Motion, Team 3058: The Jury Riggers, Team 3965: Twisted Metal, Team 4097: The Rookies</p>
- 
-		<p>Our teams placed very well, with our highest being 750 finishing 4th overall.  3965 held on to their 7th place ranking. 
-		3058 placed 9th overall.  4097 placed in 19th.</p>
+		<p>The FIRST Technology Challenge (FTC) Season began with two Kickoff Ceremonies hosted by Oregon State University and 
+		Tigard High School.  Over two hundred FTC teams were registered to compete in Oregon. They were organized into Leagues 
+		and qualifying groups and competition began in 07 November.  The four Tigard HS FTC Teams competed in the Beaverton 
+		League and acquitted themselves well, participating in four events and earning significant ranking points for the next
+		level of competition.   All four teams advanced to the League Championship Tournament where 24 teams were whittled down
+		to 12.  Ninety-six teams competed in the Super-Qualifier (think regional) and 48 advanced to the next level, the Oregon
+		State Championship Tournament, 27/28 February.  Those 48 will compete for 10 slots in the Western Super-Regional to be
+		held in Oakland, CA on 25-26 March.</p> 
 
-		<p>Based on there fourth place seeding, 750 became fourth Alliance Team Captain, choosing 3965 as 
-		their first alliance partner and another high school team as their third and final partner.
-		Team 3058 was chosen as the third alliance partner for the second Alliance team. 
-		Team 4097 was not chosen and therefore was eliminated from tournament play.</p>
+		<p>While 4097 did not make it past the League Tournament, 750, 3058 and 3965 all moved on to the 13 February SQ.  All 
+		three teams did well in inspection and judging, finishing all the preliminary events in under two hours.</p> 
 
-		<p>Alliance One vs Alliance Four started off as expected with one defeating four in the best two of three format.
-		In the second round, the team of 750/3965 pulled the upset, knocking off the first seed.  In the final and deciding
-		round, 750/3965 appeared to defeat the first alliance again.  After a significant delay while the referees measured
-		and argued and counted, the final score found 750/3965 on the wrong end of the thinnest of margins, 247-242.   750and
-		3965 were eliminated from the tournament.Meanwhile, Alliance Two with 3058 took on Alliance Three.  The alliance team
-		captain elected to sit 3058 in the first of three matches and found themselves thrashed by the third seed.
-		The team captain then elected to have his partners take the field without him and in consecutive matches,
-		A2 defeated A3 and 3058 and their partners advanced to the finals versus Alliance One.
-		Again, the A2 Captain took the field, again sitting 3058.  Again they were defeated by the other alliance.
-		The A2 Captain knew he was the weak link and played the last two matches with 3058 and a team from Wilson
-		High School.  They easily won the second match but in a close, and somewhat controversial third
-		and deciding match, A2 and 3058 defeated A1 for the League Championship Tournament title.   
-		</p>
+		<p>On the field of play, 750 and 3058 started strong, notching victories in their first two matches.  Unfortunately, 3965
+		suffered an equipment casualty, could not perform and therefore took a loss.  In the second round, 3058 again scored a
+		strong victory but 750 suffered their first defeat and 3965 again suffered an equipment casualty.  Eventually the team
+		discovered a worn wire connecting the motor controllers with the communication device.  Swapping out the damaged wire
+		solved the problem and 3965 was back in the game.</p> 
 
-		<p>
-		At the end of the day, the teams settled in for the Award Ceremony.  While some teams advance based on their
-		field play, others advance due to their body of work based on the robot, the team's success, the design, the
-		software programming, community outreach and the team presentation.  The first award handed out was the Connect
-		Award which recognizes a team for the best use of sensors and programming to defeat the problem.  3965 was one
-		of three nominees and brought home the trophy.  The next award was the Motivate Award which recognizes a team
-		for the advancement of STEM and FIRST Robotics in their school and community.  3058 was one of three nominees
-		and brought home the trophy.  We did not win any of the other category awards but we did collect one more trophy-
-		3058 picked up another for their title win. In addition to the team awards, four of our teammates competed for the
-		Dean's List Award for individual achievement.  This year's nominees included Ethan Gazin, Emma Barbee,
-		Josh Hancock, and Grace Harestad.  All four had extensive written endorsements to support their nominations
-		and during the morning judging, met with a board of three judges for interviews.  At the end of the day, all
-		four Tigard nominees were selected to advance to the State Championship level 27-28FEB.  </p>
-		<p>Tigard is also prominent in doing its part to take care of the environment. The local community is invited to donate
-		technology they no longer need to the team, and students are tasked with parting out, reusing or safely disposing of unneeded
-		technology. Through a connection TigerBytes formed with a local eCycling company (now nearing five years of partnership), 
-		our team has been responsible for providing the local community with a place to recycle technology and help keep heavy metals
-		from contaminating the earth. Applying skills learned in class, the team then builds computers to donate to needy students both
-		in the high school and throughout the community.</p>	
+		<p>In round three, 3058 again won with another impressive score.  750 suffered their own equipment casualty that caused
+		them to shut down on the field of play and they incurred their second defeat.  With new confidence in their machine,
+		3965 won their next match handily.   The fourth round saw 3058 win yet again as did 750.  3965 lost their match in a
+		close contest.</p>
+
+		<p>In the fifth and final round, 3058 won yet again, completing the seeding rounds with a 5-0 record, one of only two 5-0
+		teams, and a ranking point spread of over 100 points, making them the number one seed for the tournament.  3058 become
+		the first THS FTC team to ever sweep a tournament seeding series and become the First Alliance Team Captain.</p> 
+
+		<p>750, at 2-2, was in the middle of the pack and wanted to show that they could be a competitive choice for alliance 
+		selection.  They soundly defeated their opponents in their fifth and final match, finishing the seeding rounds 8 of
+		24.  They seemed a "shoe-in" to advance to the Elimination Rounds.</p>
+
+		<p>3965 went into the final round at 1-3, paired with the only other undefeated team in the tournament.  Unfortunately, 
+		the team everyone thought would carry 3965 to victory suffered their own casualty, falling over backwards and trapping
+		themselves on the field of play.  With no help coming from their teammate, 3965 had to count on themselves to pull out
+		the win.  And win they did, finishing the match with a score of 120-112.</p> 
+
+		<p>With the seeding rounds complete, 3058 had finished first, 750 finished 8th and 3965 ranked 18th.  Not bad!  But it 
+		wasn't over yet!</p>
+
+		<p>In the elimination rounds, four team captains (the four highest ranking teams from the seeding rounds) choose two 
+		teammates to move on with.  The remaining teams are finished for the tournament.  3058 would have the first pick as 
+		the highest seed.  If one picked two, everyone would move up and the fifth ranked team would become the fourth team 
+		captain.  As things worked out, 3058 chose the sixth team, two picked three and team five moved up to become the fourth
+		team captain. This would prove fortunate as the selections moved on.  The captains continued to choose until the new 
+		fourth team captain had to choose their final pick.  The highest ranked remaining team was 750 and we all thought they
+		would be chosen... but no.  The fourth team captain choose 3965!  They had developed a relationship with their league 
+		mates and decided to move down to the 18th place to select their final player.  Just goes to show that you can't give 
+		up, even when your robot appears to be working against you.</p>
+
+		<p>And so, Team 750 became spectators and the elimination rounds began with the first alliance taking on the fourth and 
+		the second playing against the third.  The seeding rounds predicted the outcome well.  A1, headed by 305,8 won 
+		convincingly 2-0 against an overmatched A4 with 3965.  A2 defeated A3 2-0 as well, setting up the best two out of three
+		finale between A1 and A2.</p>
+
+		<p>With seven match victories in a row under their belt, it looked like A1 with 3058 has a good chance to win the 
+		tournament but fate was not with them.  In the first round, one of their alliance partners fell over, leaving the A2
+		team with an advantage that they fully exploited, beating A1 soundly.  In the second round, all four robots performed
+		to specification and in the highest scoring match of the day, A2 defeated A1 321-290 and took the championship 2-0.</p> 
+
+		<p>Next came the award ceremony.  While 3058 collected their trophy for advancing to a second place finish, all three
+		teams were shut out of the category awards.  And then came the advancement announcements...</p>
+
+		<p>13-14 February saw four tournaments at two venues.  Each had 24 teams.  Each advanced 12 teams.  Teams are advanced 
+		based on their performance on the field but also their performance at the judging sessions.  Team 3058 advanced due 
+		to their second place finish.  Teams 750 and 3965 had to wait until the list was read.</p> 
+
+		<p>As the advancements were announced, it became more and more likely that only 3058 would advance.  I guess someone had
+		their fingers crossed for when it cam time to announce the final slot to advance to State, it fell to the "highest 
+		ranked team not yet selected", the eighth ranked team in the tournament:  Team 750.</p>
+
+		<p>And so at the end of the month, at Benson High School, Teams 750 and 3058 will try to advance again to the Western 
+		Regionals.</p>
+
+		<p>Congratulations to
+		Team 3058: Zach Carter (Team Captain), Emma Barbee, Jan Carlson, Reed Colgrove, Jason Ettenberger, Ryan Kirkpatrick, James Shea and Jalin Andrews
+		Team 750: Ethan Gazin (Team Captain), Darby Cox, Cameron Doughty, Julian, Lethbridge, Jesus Mendoza, Cao Duy Nguyen, Hoang Nguyen, Daniel Smotkin and Tyler Walker
+		Team 3965: Josh Hancock (Team Captain), Derian Rambo, Siddarth Sylace, Ethan Vo and Derek Williams</p>
+
+		<p>A special thanks to the twelve Tech Team coaches and parents who turned out at the tournament venues Saturday and 
+		Sunday to make things happen.  Without them serving as inspectors, referees, judges and administrative volunteers, our
+		tournament and those played at other venues would not have been possible.  Thank you all!</p>	
 	</div>
 	<div class="maint-right">
 		<br/><br/><br/>
