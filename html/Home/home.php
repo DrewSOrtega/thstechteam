59a18d570c2f0b064566fed65fe789ae9b8f35af
fluteguy400 <fluteguy400@gmail.com>
2016-02-23 17:56:49 -0800

    Updated from intentions message

--- html/Home/home.php
+++ html/Home/home.php
@@ -28,87 +28,17 @@ img{
 <div class="maint"><div class="bodytext">
 	<div class="maint-left">
 		<h2 align="left">News:</h2>
-		<p>The FIRST Technology Challenge (FTC) Season began with two Kickoff Ceremonies hosted by Oregon State University and 
-		Tigard High School.  Over two hundred FTC teams were registered to compete in Oregon. They were organized into Leagues 
-		and qualifying groups and competition began in 07 November.  The four Tigard HS FTC Teams competed in the Beaverton 
-		League and acquitted themselves well, participating in four events and earning significant ranking points for the next
-		level of competition.   All four teams advanced to the League Championship Tournament where 24 teams were whittled down
-		to 12.  Ninety-six teams competed in the Super-Qualifier (think regional) and 48 advanced to the next level, the Oregon
-		State Championship Tournament, 27/28 February.  Those 48 will compete for 10 slots in the Western Super-Regional to be
-		held in Oakland, CA on 25-26 March.</p> 
+		<p>FRC will put the robot in the bag and call it a day for another build season.</p> 
 
-		<p>While 4097 did not make it past the League Tournament, 750, 3058 and 3965 all moved on to the 13 February SQ.  All 
-		three teams did well in inspection and judging, finishing all the preliminary events in under two hours.</p> 
+		<p>The backup robot will continue preps and testing.</p> 
 
-		<p>On the field of play, 750 and 3058 started strong, notching victories in their first two matches.  Unfortunately, 3965
-		suffered an equipment casualty, could not perform and therefore took a loss.  In the second round, 3058 again scored a
-		strong victory but 750 suffered their first defeat and 3965 again suffered an equipment casualty.  Eventually the team
-		discovered a worn wire connecting the motor controllers with the communication device.  Swapping out the damaged wire
-		solved the problem and 3965 was back in the game.</p> 
+		<p>This is the also the week of the FTC State Championships!  ORTOP has been paid.  750 and 3058 are assigned to the 
+		22 team Timber Division.  Tech Division is where the other 22 teams are lodged. Four teams have been dropped.  Don't 
+		know why...</p>
 
-		<p>In round three, 3058 again won with another impressive score.  750 suffered their own equipment casualty that caused
-		them to shut down on the field of play and they incurred their second defeat.  With new confidence in their machine,
-		3965 won their next match handily.   The fourth round saw 3058 win yet again as did 750.  3965 lost their match in a
-		close contest.</p>
+		<p>Some stiff competition ahead!  Of the 44 teams still standing, and estimated 10 teams (ORTOP has not released the
+		final number) will be advancing to the Western Regionals in Oakland.  I hope we are ready!</p>
 
-		<p>In the fifth and final round, 3058 won yet again, completing the seeding rounds with a 5-0 record, one of only two 5-0
-		teams, and a ranking point spread of over 100 points, making them the number one seed for the tournament.  3058 become
-		the first THS FTC team to ever sweep a tournament seeding series and become the First Alliance Team Captain.</p> 
-
-		<p>750, at 2-2, was in the middle of the pack and wanted to show that they could be a competitive choice for alliance 
-		selection.  They soundly defeated their opponents in their fifth and final match, finishing the seeding rounds 8 of
-		24.  They seemed a "shoe-in" to advance to the Elimination Rounds.</p>
-
-		<p>3965 went into the final round at 1-3, paired with the only other undefeated team in the tournament.  Unfortunately, 
-		the team everyone thought would carry 3965 to victory suffered their own casualty, falling over backwards and trapping
-		themselves on the field of play.  With no help coming from their teammate, 3965 had to count on themselves to pull out
-		the win.  And win they did, finishing the match with a score of 120-112.</p> 
-
-		<p>With the seeding rounds complete, 3058 had finished first, 750 finished 8th and 3965 ranked 18th.  Not bad!  But it 
-		wasn't over yet!</p>
-
-		<p>In the elimination rounds, four team captains (the four highest ranking teams from the seeding rounds) choose two 
-		teammates to move on with.  The remaining teams are finished for the tournament.  3058 would have the first pick as 
-		the highest seed.  If one picked two, everyone would move up and the fifth ranked team would become the fourth team 
-		captain.  As things worked out, 3058 chose the sixth team, two picked three and team five moved up to become the fourth
-		team captain. This would prove fortunate as the selections moved on.  The captains continued to choose until the new 
-		fourth team captain had to choose their final pick.  The highest ranked remaining team was 750 and we all thought they
-		would be chosen... but no.  The fourth team captain choose 3965!  They had developed a relationship with their league 
-		mates and decided to move down to the 18th place to select their final player.  Just goes to show that you can't give 
-		up, even when your robot appears to be working against you.</p>
-
-		<p>And so, Team 750 became spectators and the elimination rounds began with the first alliance taking on the fourth and 
-		the second playing against the third.  The seeding rounds predicted the outcome well.  A1, headed by 305,8 won 
-		convincingly 2-0 against an overmatched A4 with 3965.  A2 defeated A3 2-0 as well, setting up the best two out of three
-		finale between A1 and A2.</p>
-
-		<p>With seven match victories in a row under their belt, it looked like A1 with 3058 has a good chance to win the 
-		tournament but fate was not with them.  In the first round, one of their alliance partners fell over, leaving the A2
-		team with an advantage that they fully exploited, beating A1 soundly.  In the second round, all four robots performed
-		to specification and in the highest scoring match of the day, A2 defeated A1 321-290 and took the championship 2-0.</p> 
-
-		<p>Next came the award ceremony.  While 3058 collected their trophy for advancing to a second place finish, all three
-		teams were shut out of the category awards.  And then came the advancement announcements...</p>
-
-		<p>13-14 February saw four tournaments at two venues.  Each had 24 teams.  Each advanced 12 teams.  Teams are advanced 
-		based on their performance on the field but also their performance at the judging sessions.  Team 3058 advanced due 
-		to their second place finish.  Teams 750 and 3965 had to wait until the list was read.</p> 
-
-		<p>As the advancements were announced, it became more and more likely that only 3058 would advance.  I guess someone had
-		their fingers crossed for when it cam time to announce the final slot to advance to State, it fell to the "highest 
-		ranked team not yet selected", the eighth ranked team in the tournament:  Team 750.</p>
-
-		<p>And so at the end of the month, at Benson High School, Teams 750 and 3058 will try to advance again to the Western 
-		Regionals.</p>
-
-		<p>Congratulations to
-		Team 3058: Zach Carter (Team Captain), Emma Barbee, Jan Carlson, Reed Colgrove, Jason Ettenberger, Ryan Kirkpatrick, James Shea and Jalin Andrews
-		Team 750: Ethan Gazin (Team Captain), Darby Cox, Cameron Doughty, Julian, Lethbridge, Jesus Mendoza, Cao Duy Nguyen, Hoang Nguyen, Daniel Smotkin and Tyler Walker
-		Team 3965: Josh Hancock (Team Captain), Derian Rambo, Siddarth Sylace, Ethan Vo and Derek Williams</p>
-
-		<p>A special thanks to the twelve Tech Team coaches and parents who turned out at the tournament venues Saturday and 
-		Sunday to make things happen.  Without them serving as inspectors, referees, judges and administrative volunteers, our
-		tournament and those played at other venues would not have been possible.  Thank you all!</p>	
 	</div>
 	<div class="maint-right">
 		<br/><br/><br/>
