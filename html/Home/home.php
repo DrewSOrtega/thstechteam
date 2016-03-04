06ff33080f39facabf7fdf6c53fb9abbbdca7d9f
Drew S. Ortega <Drewsortega@gmail.com>
2016-03-03 17:42:25 -0800

    Update home.php

--- html/Home/home.php
+++ html/Home/home.php
@@ -28,19 +28,40 @@ img{
 <div class="maint"><div class="bodytext">
 	<div class="maint-left">
 		<h2 align="left">News:</h2>
-		<p>FRC will put the robot in the bag and call it a day for another build season.</p> 
+		<p>Another busy weekend!  Members of the Tech Team attended the Durham Elementary STEM Fair for the second time Friday night.  The number of participants at this year's fair was double what we saw last year.  I guess the word got out that we were attending!  Teams 750 and 3058 as well as elements of Team 3192 drove robots, conducted demonstrations and best of all, allowed the youngest of robot enthusiasts to drive some of our most complex machines including TWAIN.  Well done Team!</p>
 
-		<p>The backup robot will continue preps and testing.</p> 
+		<p>Of course, we wrapped up quickly because it was all to begin again on Saturday morning as the FTC Teams 750 and 3058 began their run at the State Championship.  The teams arrived early at Benson so that we could complete our traditional job of building the competition fields.  Inspection went off without a hitch and the two judging sessions left the teams confident that they had done a good job.  We departed the scene early and everyone went home for a well-earned night off. </p>
 
-		<p>This is the also the week of the FTC State Championships!  ORTOP has been paid.  750 and 3058 are assigned to the 
-		22 team Timber Division.  Tech Division is where the other 22 teams are lodged. Four teams have been dropped.  Don't 
-		know why...</p>
+	<p>Sunday we returned to Benson and began the competition in earnest.  Forty-four teams were divided into two divisions of twenty-two teams each.  Teams 750 and 3058 were assigned to the Timber Division.  They also found that they were the longest continuously active teams left in the competition.  But old age would not be a guarantee of success...</p>
 
-		<p>Some stiff competition ahead!  Of the 44 teams still standing, and estimated 10 teams (ORTOP has not released the
-		final number) will be advancing to the Western Regionals in Oakland.  I hope we are ready!</p>
-		
-		<p>The IT program recently got a grant from the government, ensuring that all of our Certification tests are now 
-		free, (Yes, including the A+)</p>
+	<p>We began the day with the Opening Ceremonies which included the introduction of the sixteen Oregon Finalists for the Dean's List.  In an effort to recognize the leadership and
+	dedication of FIRST’s most outstanding FTC students, the Kamen family sponsors an Award for selected top students known as the FIRST Dean’s List.  Since its introduction in
+	2010, the FIRST Dean’s List Award has attracted the attention of prestigious colleges and universities who desire to recruit FIRST Dean’s List students.  There are three (3) “categories” of FIRST Dean’s List Award students:
+	1. FIRST Dean’s List  Semi - Finalists are the students nominated by each Team to compete for the FIRST Dean’s List Finalist spots.
+	2. FIRST Dean’s List Finalists - The students selected to be recognized at each State/Regions Championship that are in the running for the FIRST Dean’s List Winner spots.
+	3. FIRST Dean’s List Winners - The group of 10 students who are the final selection for the Dean’s List Award at the FIRST World Championship</p>
+
+	<p>This year, the THS FTC teams nominated Ethan Gazin, Emma Barbee, Josh Hancock and Grace Harestad for the Dean's list.  All four completed individual interviews at competitions during the FTC robotics season.  All four were selected to advance to the Oregon State Championship round as Oregon Finalist contestants.</p> 
+
+	<p>After the Opening Ceremonies, we began the actual competition with 44 of the 48 top FTC teams in Oregon in attendance.  With two divisions running simultaneously, the floor of the Benson High School gym was chaotic but overall, the event ran smoothly.  Unfortunately, the same could not be said about our teams.  At the end of the day, 750, who had acquitted themselves well throughout the season, could not buy a win, coming up short on the score five times and finishing the seeding rounds 0-5 and 22 of 22 in the championship seeding.  3058, who has won seven consecutive matches to win their way into the State Championship Tourney faired little better.  At the end of the day, 3058 finished 1-4 and ranked 17 of 22.  While they had accumulated over 700 ranking points, the most of any team in the tourney and demonstrating their ability to score and score often, their 1-4 record did not look good. </p>
+
+	<p>At the Alliance selection, it looked like 3058 might still advance to the elimination rounds as the last two alliance captains could not decide which of the remaining 12 teams they could select from would be the most effective for their alliance.  3058 was passed over and the season for the THS FTC teams came to an end.</p>
+
+	<p>The teams thought they may have a shot at one of the category awards and we did not know if one or all of our Dean's List Finalists would advance to Worlds so we stayed for the award ceremony. In the first few minutes, the Dean's List Finalists were announced.  Oregon could advance four students to the World Championships...  Emma Barbee of Team 3058 will be one of them!</p>
+
+	<p>The rest of the evening was anti-climatic as we failed to win any of the category awards.  We were encouraged by the fact that Oregon would advance fourteen teams to the Western Regionals, the largest contingent from any of the 11 states competing in Oakland.  More than Washington.  More than California.  Yes, the competition is deep in Oregon, home of the last two World Championship teams.</p>
+
+	<p>It has been a great season!  48 students joined four FTC teams to move through league play in November and December along with over 200 teams with almost 3000 students state-wide.  Three of the THS teams advanced from the League Championship Tournament in January.  Two advanced to the State Championship from the Super-Qualifier at the beginning of February.  While we did not make it to the Western Regionals, we did learn much about engineering design, the iterative build process, programming in a JAVA Application environment, power maintenance and distribution, motor and gear assemblies and of course, the tensile strength of tape measures.</p>
+
+	<p>Our THS FTC State Championship teams:</p>
+
+	<p>Team 750, Gears in Motion, Ethan Gazin (Team Captain and Dean's List Semi-Finalist), Darby Cox, Cameron Doughty, Ryan Hardinge, Julian Lethbridge, Jesus Mendoza, Cao-Duy Nguyen, Hoang Nguyen, Daniel Smotkin and Tyler Walker.</p>
+
+	<p>Team 3058, The Jury Riggers, Zach Carter (Team Captain), Jalin Andrews, Emma Barbee (Dean's List Oregon Finalist), Jan Carlson, Reed Colgrove, Jason Ettenberger, Ryan Kirkpatrick and James Shea</p>
+
+	<p>While the FTC season is now complete, the FRC season is about to heat up with three competitions in the month of March.  The scores from two of those events will be added together to determine if FRC Team 3192, the Tiger Bytes, will advance to the Pacific Northwest Regional in April.</p>
+
+	<p>Let the next round of robotics competitions begin!</p>
 
 	</div>
 	<div class="maint-right">
