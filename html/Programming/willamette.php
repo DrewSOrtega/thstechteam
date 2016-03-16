c842cef26953420cc875aec3eed197abab51a885
fluteguy400 <fluteguy400@gmail.com>
2016-03-15 18:55:10 -0700

    Update willamette.php

--- html/Programming/willamette.php
+++ html/Programming/willamette.php
@@ -28,7 +28,7 @@ img{
 <h2>Willamette University</h2>
 <p>The Willamette programming challenge is about a team of highschool students, competing to complete a set of challenges in a set amount of time. The challenges vary from year to year, varying in difficulty, practicality, and the number of them. Each team brings one computer and attempts to solve it. They are judged on practicality design and formatting of their program, if they fail to solve the problem at first they are allowed to resubmit it without penalty. At the end of it the team with the most points wins and the top three teams earn a trophy and so does one &quot;novice&quot; team, A novice team is defined as one who just entered that year. Every highschool in Oregon is invited to join. The registration starts at 0830 of the day, the contest starts at 1000 and ends at 1500. This contest was designed to encourage teamwork. It requires collaboration and understanding of each team. The students must learn to work together and get a program done, whether this be through troubleshooting, rough drafts, or anything of that kind.</p>
 </br>
-<p>We will be sending two teams (One Varsity Team and one JV Team) to this years competition, each with four members. The members are as follows:</p>
+<p>We will be sending two teams (One Varsity Team and one JV Team) to this years competition. The members are as follows:</p>
 <br>
 <h2>Roster</h2>
 <ul>
